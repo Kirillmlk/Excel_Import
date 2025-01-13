3689d4a9dbdd7e3851040985ab86893f86a5c2ab
Kirillmlk <kostakov.kiirill@yandex.by>
2025-01-13 23:13:19 +0300

    Adding routes, controllers, and components

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<script>
+import {Link} from "@inertiajs/vue3";
+export default {
+    name: "MainLayout",
+    components: {
+        Link
+    }
+}
+</script>
+
+<template>
+    <div class="w-96 mx-auto">
+        <h1>My App</h1>
+        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+            <Link :href="route('dashboard')">
+                Dashboard
+            </Link>
+
+            <Link :href="route('project.index')">
+                Projects
+            </Link>
+
+            <Link :href="route('project.import')">
+                Import
+            </Link>
+
+            <Link :href="route('task.index')">
+                Tasks
+            </Link>
+        </div>
+        <slot>
+
+        </slot>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Project/Index.vue
@@ -0,0 +1,18 @@
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+export default {
+    name: "Index",
+
+    layout: MainLayout,
+}
+</script>
+
+<template>
+  <div>
+    <h1>Index</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
